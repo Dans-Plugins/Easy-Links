d7147c413740f6fc8ebe44954f674b1b2c6cc34d
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-12 19:53:47 -0700

    initial commit

---
+++ src/main/java/dansplugins/easylinks/EasyLinks.java
@@ -0,0 +1,17 @@
+package dansplugins.easylinks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyLinks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
