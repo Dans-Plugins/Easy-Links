19ac4ab4ac7f9702b50ad3b504bc2f2cb7970dfe
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-14 23:21:53 -0700

    Implemented PersistentData.java

--- src/main/java/dansplugins/easylinks/data/PersistentData.java
+++ src/main/java/dansplugins/easylinks/data/PersistentData.java
@@ -1,5 +1,45 @@
 package dansplugins.easylinks.data;
 
+import dansplugins.easylinks.objects.Link;
+
+import java.util.HashSet;
+
 public class PersistentData {
-    // TODO: implement
+
+    private static PersistentData instance;
+
+    private HashSet<Link> links = new HashSet<>();
+
+    private PersistentData() {
+
+    }
+
+    public PersistentData getInstance() {
+        if (instance == null) {
+            instance = new PersistentData();
+        }
+        return instance;
+    }
+
+    public HashSet<Link> getLinks() {
+        return links;
+    }
+
+    public Link getLink(String label) {
+        for (Link link : links) {
+            if (link.getLabel().equalsIgnoreCase(label)) {
+                return link;
+            }
+        }
+        return null;
+    }
+
+    public boolean addLink(Link link) {
+        return links.add(link);
+    }
+
+    public boolean removeLink(Link link) {
+        return links.remove(link);
+    }
+
 }
