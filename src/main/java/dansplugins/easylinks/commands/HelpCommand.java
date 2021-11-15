79a54b7caf41d4b18ec3523713ae9df2ce159510
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-14 23:37:20 -0700

    Altered HelpCommand.java again

--- src/main/java/dansplugins/easylinks/commands/HelpCommand.java
+++ src/main/java/dansplugins/easylinks/commands/HelpCommand.java
@@ -24,11 +24,11 @@ public class HelpCommand extends AbstractCommand {
 
     @Override
     public boolean execute(CommandSender commandSender) {
-        commandSender.sendMessage(ChatColor.AQUA + "/el help");
-        commandSender.sendMessage(ChatColor.AQUA + "/el delete");
-        commandSender.sendMessage(ChatColor.AQUA + "/el edit");
-        commandSender.sendMessage(ChatColor.AQUA + "/el view");
         commandSender.sendMessage(ChatColor.AQUA + "/el list");
+        commandSender.sendMessage(ChatColor.AQUA + "/el help");
+        commandSender.sendMessage(ChatColor.AQUA + "/el view");
+        commandSender.sendMessage(ChatColor.AQUA + "/el create");
+        commandSender.sendMessage(ChatColor.AQUA + "/el delete");
         commandSender.sendMessage(ChatColor.AQUA + "/el stats");
         return true;
     }
